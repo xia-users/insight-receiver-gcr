8db6a21a1fcc391f2202534fc838bbedb52fbb48
Soral <cx.soral@gmail.com>
2021-03-14 11:55:10 +0100

    Alpha release

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,13 @@ ENV PYTHONUNBUFFERED True
 ENV APP_HOME /app
 WORKDIR $APP_HOME
 COPY LICENSE LICENSE
+COPY requirements.txt requirements.txt
+COPY config config
 COPY static static
 COPY templates templates
 COPY main.py main.py
 # Install production dependencies.
-RUN pip install Flask gunicorn xialib xialib-gcp pyinsight google-cloud-logging
+RUN pip install -r requirements.txt
 # Run the web service on container startup. Here we use the gunicorn
 # webserver, with one worker process and 8 threads.
 # For environments with multiple CPU cores, increase the number of workers
--- main.py
+++ main.py
@@ -7,15 +7,13 @@ from flask import Flask, request, Response, render_template
 import google.cloud.logging
 import google.auth
 from xialib.service import service_factory
-from pyinsight import Insight, Receiver
+
 
 # Global Setting
 app = Flask(__name__)
 project_id = google.auth.default()[1]
 
 # Configuration Load
-with open(os.path.join('.', 'config', 'insight_config.json')) as fp:
-    insight_config = json.load(fp)
 with open(os.path.join('.', 'config', 'global_conn_config.json')) as fp:
     global_conn_config = json.load(fp)
 with open(os.path.join('.', 'config', 'object_config.json')) as fp:
@@ -23,28 +21,18 @@ with open(os.path.join('.', 'config', 'object_config.json')) as fp:
 
 # Global Object Factory
 global_connectors = service_factory(global_conn_config)
-insight_messager = service_factory(insight_config['messager'], global_connectors)
-Insight.set_internal_channel(messager=insight_messager,
-                             channel=insight_config.get('control_channel', project_id),
-                             topic_cockpit=insight_config['control_topics']['cockpit'],
-                             topic_cleaner=insight_config['control_topics']['cleaner'],
-                             topic_merger=insight_config['control_topics']['merger'],
-                             topic_packager=insight_config['control_topics']['packager'],
-                             topic_loader=insight_config['control_topics']['loader'],
-                             topic_backlog=insight_config['control_topics']['backlog']
-)
 
 # Log configuration
 client = google.cloud.logging.Client()
 client.get_default_handler()
 client.setup_logging()
 
+
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    if request.method == 'GET':
-        receiver = service_factory(object_config, global_connectors)
-        return render_template("index.html"), 200
     receiver = service_factory(object_config, global_connectors)
+    if request.method == 'GET':
+        return render_template("index.html"), 200
 
     envelope = request.get_json()
     if not envelope:
